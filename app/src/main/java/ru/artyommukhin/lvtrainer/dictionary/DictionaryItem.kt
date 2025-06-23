9ad2a9cc46be3c59574e89ac53a7ac43e6c01183
artyommukhin <artyom.mukhin@gmail.com>
2025-06-23 23:59:18 +0900

    Fix DictionaryItem layout when text is too wide

--- app/src/main/java/ru/artyommukhin/lvtrainer/dictionary/DictionaryItem.kt
+++ app/src/main/java/ru/artyommukhin/lvtrainer/dictionary/DictionaryItem.kt
@@ -2,8 +2,8 @@ package ru.artyommukhin.lvtrainer.dictionary
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Refresh
@@ -13,6 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import ru.artyommukhin.lvtrainer.dictionary.data.DictionaryWord
 
@@ -29,11 +30,12 @@ fun DictionaryItem(
         verticalAlignment = Alignment.CenterVertically,
     ) {
         Text(
+            modifier = Modifier
+                .padding(end = 8.dp)
+                .weight(1f),
             fontSize = 24.sp,
             text = "${word.word} - ${word.translation}",
         )
-
-        Spacer(Modifier.weight(1f))
 
         Text("${word.trainCount * 20}%")
 
