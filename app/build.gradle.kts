6a058771dd4c60f02d2341bcf5f46dd3341e6638
artyommukhin <artyom.mukhin@gmail.com>
2026-09-23 18:27:15 +1100

    Update version to 0.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ extensions.configure<ApplicationExtension> {
         applicationId = "ru.artyommukhin.lvtrainer"
         minSdk = 23
         targetSdk = 37
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
